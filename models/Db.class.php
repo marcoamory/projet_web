7b2487f80bba860484984142e221fe8f0b2a1a32
ORL-senpai <robinworldw@hotmail.com>
2017-04-24 12:09:07 +0200

    test

---
+++ models/Db.class.php
@@ -0,0 +1,25 @@
+﻿<?php
+class Db{
+	private $_db;
+	private static $instance = null;
+	
+	public static function getInstance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new Db();
+		}
+		return self::$instance;
+	}
+	
+	private function __construct() {
+		try{
+			$this->_db=new PDO('mysql:host=localhost;dbname=sitepersodb;charset=utf8','root','');
+			$this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			$this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+		}
+		catch (PDOException $e) {
+			die('Erreur de connexion à la base de données : '.
+			$e->getMessage());
+		}
+	}
+}
+?>
