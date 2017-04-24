79e1ee792d16129be06f086d4c265fcacdcfaee9
marcoamory <amorymarco@gmail.com>
2017-04-24 12:32:06 +0200

    changement view -> views

---
+++ views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<meta charset="utf-8"/>
+		<meta name="authors" content="Robin Louis, Marco Amory"/>
+		<meta name="description" content="Application de prise de présence de l'IPL"/>
+      	<link rel="stylesheet" type="text/css" href="views/css/bootstrap.min.css">
+      	<link rel="stylesheet" type="text/css" href="views/css/bootstrap-theme.min.css">
+      	<link rel="stylesheet" type="text/css" href="views/css/font-awesome.min.css">
+	</head>
+
+	<body>
+		<header>
+
+		</header>
+		<nav>
+
+		</nav>
+
