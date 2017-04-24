19368647200e63d6a2c5b6973e5f25a37e4f9809
marcoamory <amorymarco@gmail.com>
2017-04-24 11:47:01 +0200

    Header

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+session_start();
+
+define("PATH_VIEW", "views/");
+define("PATH_CONTROLLER", "controllers/");
+define("PATH_MODEL", "models/");
+define("PATH_CSS", PATH_VIEW . "css/");
+define("PATH_IMAGE", PATH_VIEW . "images/");
+
+if (empty($_SESSION['authentifie'])) {
+   header("Location: index.php?action=login");
+	die();
+}
