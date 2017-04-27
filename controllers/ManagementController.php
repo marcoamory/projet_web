68ca121d4de23c7ae25c2c7d76916d6eba6c49f6
marcoamory <amorymarco@gmail.com>
2017-04-27 11:38:12 +0200

    Correction conflit

---
+++ controllers/ManagementController.php
@@ -0,0 +1,20 @@
+<?php
+class ManagementController{
+	private $_db;
+
+	public function __construct($db) {
+		$this->_db = $db;
+	}
+
+	public function run(){
+		var_dump($_POST);
+		if(isset($_POST['lessonType'])){
+			if($_POST['lessonType']=="LearningActivity")
+				$this->_db->insertLesson($_POST['lessonName'], "LA", $_POST['lessonQuad'], $_POST['lessonCred'], $_POST['lessonAb']);
+			if($_POST['lessonType']=="CourseUnit")
+				$this->_db->insertLesson($_POST['lessonName'], "CU", $_POST['lessonQuad'], $_POST['lessonCred'], $_POST['lessonAb']);
+		}
+		require_once(PATH_VIEW . 'blocManager.php');
+	}
+}
+?>
