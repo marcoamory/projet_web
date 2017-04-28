96455218e5325c75f1bbda39f57ad96fc5384b6e
marcoamory <amorymarco@gmail.com>
2017-04-28 09:29:28 +0200

    Modification underscore + variable db

--- models/Db.class.php
+++ models/Db.class.php
@@ -22,7 +22,7 @@ class Db{
 		}
 	}
 
-	public function insertWeek($week_number, $name, $monday_date, $quadri)
+	public function insert_week($week_number, $name, $monday_date, $quadri)
 	{
 		$req = $this->_db->prepare('INSERT INTO weeks (week_number, name, monday_date, quadri) VALUES (:week_number, :name, :monday_date, :quadri)');
 		$req->execute(array('week_number' => $week_number,
@@ -31,11 +31,11 @@ class Db{
 							'quadri' => $quadri));
 	}
 
-	public function insertTeacher($email_teacher, $name, $last_name, $responsability)
+	public function insert_teacher($email_teacher, $name, $last_name, $responsability)
 	{
-		$req = $this->_db->prepare('INSERT INTO teachers (email_teacher, name, last_name, responsability) VALUES (:email_teacher, :name, :last_name, :responsability)');
+		$req = $this->_db->prepare('INSERT INTO teachers (email_teacher, first_name, last_name, responsability) VALUES (:email_teacher, :first_name, :last_name, :responsability)');
 		$req->execute(array('email_teacher' => $email_teacher,
-							'name' => $name,
+							'first_name' => $name,
 							'last_name' => $last_name,
 							'responsability' => $responsability));
 	}
@@ -47,10 +47,10 @@ class Db{
 	{
 
 		$req = $this->_db->prepare('INSERT INTO students (bloc, last_name, first_name, email_student) VALUES (:bloc, :last_name, :first_name, :email_student)');
-		$req->execute(array('bloc' => $email_student,
+		$req->execute(array('email_student' => $email_student,
 							'last_name' => $last_name,
-							'first_name' => $last_name,
-							'email_student' => $bloc));
+							'first_name' => $first_name,
+							'bloc' => $bloc));
 	}
 	
 	public function insert_lesson($name, $lesson_code, $quadri, $type, $credits, $abbreviation)
@@ -63,28 +63,28 @@ class Db{
 							'credits' => $credits,
 							'abbreviation' => $abbreviation));
 	}
-	public function insertSerie($number,$bloc)
+	public function insert_serie($number,$bloc)
 	{
 		/*here the primary key cannot be automaticaly incremented since the primary key is number and bloc binded together 
 		 *ie:serie1,bloc1 / serie1,bloc2
 		 */
-		$req = $this->_db->prepare('INSERT INTO series (number,bloc) VALUES (:number, :bloc)');
-		$req->execute(array('number' => $number,
+		$req = $this->_db->prepare('INSERT INTO series (number_serie,bloc) VALUES (:number_serie, :bloc)');
+		$req->execute(array('number_serie' => $number_serie,
 							'bloc' => $bloc));
 	}
 
-	public function searchStudent($email)
+	public function search_student($email)
 	{
-		$req = $this->_db->prepare("SELECT email_student, name, last_name, number, bloc FROM students WHERE email_student = :email_student");
+		$req = $this->_db->prepare("SELECT email_student, first_name, last_name, number, bloc FROM students WHERE email_student = :email_student");
 		$req->execute(array("email_student" => $email));
 		$result = $req->fetch();
 		$req->closeCursor();
 		return $result;
 	}
 
-	public function searchTeacher($email)
+	public function search_teacher($email)
 	{
-		$req = $this->_db->prepare("SELECT email_teacher, name, last_name, responsibility FROM teachers WHERE email_teacher = :email_teacher");
+		$req = $this->_db->prepare("SELECT email_teacher, first_name, last_name, responsibility FROM teachers WHERE email_teacher = :email_teacher");
 		$req->execute(array("email_teacher" => $email));
 		$result = $req->fetch();
 		$req->closeCursor();
