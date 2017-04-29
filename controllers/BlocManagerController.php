c9f61a2346f29a32fad35acea1670de7a280b897
ORL-senpai <robinworldw@hotmail.com>
2017-04-29 13:59:34 +0200

    commit de pré-sync

--- controllers/BlocManagerController.php
+++ controllers/BlocManagerController.php
@@ -8,6 +8,7 @@ class BlocManagerController{
 	}
 
 	public function run(){
+		var_dump(!$this->_db->search_lesson("I1020"));
 		if(isset($_SESSION['notificationError']))
 			$_SESSION['notificationError']='';
 		if(isset($_SESSION['notificationSuccess']))
@@ -41,7 +42,7 @@ class BlocManagerController{
 	
 	public function define_pattern($uploadName){
 		if($uploadName=='students_csv')
-			return "(.*);(.*);(.*);(.*)\n$";
+			return "(Bl.*);(.*);(.*);(.*)\n$";
 		return "(.*);(.*);(.*);(.*);(.*);(.*)\n$";
 	}
 	
@@ -66,11 +67,20 @@ class BlocManagerController{
 	
 	/* créer une fonction qui vérifie si ils ne sont pas déjà dans la base de donnée
 	 * méditer sur : le faire via try/catch ou alors un for qui fait un select sur la donnée courrante du fichier pour vérifier
-	 * si elle n'existe pas déjà, il suffit de vérifier la pk pas le reste.
-	public function is_being_duplicated{//il pourrait renvoyer un tableau des éléments qui sont dupliqués pour renvoyer à la vue telle telle et telle données sont déjà présentes
-	
+	 * si elle n'existe pas déjà, il suffit de vérifier la pk pas le reste.*/
+	public function is_being_duplicated($primaryKey, $keyValue){//il pourrait renvoyer un tableau des éléments qui sont dupliqués pour renvoyer à la vue telle telle et telle données sont déjà présentes
+		if($primaryKey=='email_student'){
+			if(!$this->_db->search_student($keyValue));
+				return true;
+		}
+		elseif($primaryKey=='lesson_code'){
+			if(!$this->_db->search_lesson($keyValue));
+				return true;
+		}
+		else
+			return false;
 	}
-	 */
+	 
 		
 	// si c'est un RBs il faut qu'il puisse ajouter le bloc dans lequel il veut l'ajouter et vérifier que cela correspond bien au 2 premieres lettres 
 	// de lesson_code du cours en question.
@@ -81,11 +91,13 @@ class BlocManagerController{
 			$line = $arrayFile[$i];	
 			if($uploadName=='students_csv'){
 				if(preg_match("/".$pattern."/", $line, $groups))
-					$this->_db->insert_student($groups[1], $groups[2], $groups[3], $groups[4]);
+					//if(!$this->is_being_duplicated('email_student',$groups[4]))
+						$this->_db->insert_student($groups[1], $groups[2], $groups[3], $groups[4]);
 			}
 			else if ($uploadName=='lessons_csv'){
 				if(preg_match("/".$pattern."/", $line, $groups))
-					$this->_db->insert_lesson($groups[1], $groups[2], $groups[3], $groups[4], $groups[5], $groups[6]);
+					//if(!$this->is_being_duplicated('lesson_code',$groups[2]))
+						$this->_db->insert_lesson($groups[1], $groups[2], $groups[3], $groups[4], $groups[5], $groups[6]);
 			}
 		}
 
