c816ef3655b04b9d57f22a9b0d8bb473353d2d73
ORL-senpai <robinworldw@hotmail.com>
2017-05-01 15:48:13 +0200

    update Admin
    J'ai rajouté la fonctionnalité de wipe des données dans admin que j'ai
    du faire en responsable de blocS comme elle est commune du coup, fais
    gaffe elle wipe tout, profs compris. je dois demander aux profs si il
    faut décider des données qu'on wipe ou si on wipe tout, c'est pas clair
    dans l'enoncé

--- controllers/AdminManagementController.php
+++ controllers/AdminManagementController.php
@@ -8,7 +8,6 @@ class AdminManagementController{
 
 		$this->_db = $db;
 	}
-
 	function run(){
 
 		$this->process_file('professor_csv');
@@ -16,34 +15,42 @@ class AdminManagementController{
 		require_once(PATH_VIEW.'adminManagement.php');
 
 	}
-		public function process_file($uploadName){
-			if(isset($_FILES[$uploadName])){
-				$tmp_name=$_FILES[$uploadName]['tmp_name'];
-				$name=$_FILES[$uploadName]['name'];
-				move_uploaded_file($tmp_name, PATH_CONF.$name);
-				$this->file_to_DB($uploadName,$name);
+	
+	public function wipe_data(){
+		$this->_db->drop_all_data();
+	}
+
+	public function process_file($uploadName){
+		if(isset($_FILES[$uploadName])){
+			$tmp_name=$_FILES[$uploadName]['tmp_name'];
+			$name=$_FILES[$uploadName]['name'];
+			move_uploaded_file($tmp_name, PATH_CONF.$name);
+			$this->file_to_DB($uploadName,$name);
+		}
+		if(!isset($_FILES['professor_csv'])&&!isset($_POST['agenda_properties'])&&isset($_POST['wipeChoice'])){
+			$this->wipe_data();
+		}
+	}
+
+	public function file_to_DB($uploadName,$name){
+		if($uploadName=='professor_csv')
+			$pattern = "(.*);(.*);(.*);(.*)\n$";
+		else if ($uploadName=='lessons_csv')
+			$pattern = "(.*);(.*);(.*);(.*);(.*);(.*)\n$";
+		$arrayFile = file(PATH_CONF . $name);
+		$nb_lines = count($arrayFile);
+		for($i = 1; $i < $nb_lines; $i++){
+			$line = $arrayFile[$i];	
+			if($uploadName=='professor_csv'){
+				if(preg_match("/".$pattern."/", $line, $groups))
+					var_dump($groups);
+					$this->_db->insert_teacher($groups[1], $groups[2], $groups[3], $groups[4]);
+			}
+			else if ($uploadName=='agenda_properties'){
+				if(preg_match("/".$pattern."/", $line, $groups))
+					$this->_db->insert_week($groups[1], $groups[2], $groups[3], $groups[4], $groups[5], $groups[6]);
 			}
 		}
 
-		public function file_to_DB($uploadName,$name){
-			if($uploadName=='professor_csv')
-				$pattern = "(.*);(.*);(.*);(.*)\n$";
-			else if ($uploadName=='lessons_csv')
-				$pattern = "(.*);(.*);(.*);(.*);(.*);(.*)\n$";
-			$arrayFile = file(PATH_CONF . $name);
-			$nb_lines = count($arrayFile);
-			for($i = 1; $i < $nb_lines; $i++){
-				$line = $arrayFile[$i];	
-				if($uploadName=='professor_csv'){
-					if(preg_match("/".$pattern."/", $line, $groups))
-						var_dump($groups);
-						$this->_db->insert_teacher($groups[1], $groups[2], $groups[3], $groups[4]);
-				}
-				else if ($uploadName=='agenda_properties'){
-					if(preg_match("/".$pattern."/", $line, $groups))
-						$this->_db->insert_week($groups[1], $groups[2], $groups[3], $groups[4], $groups[5], $groups[6]);
-				}
-			}
-
-		}
+	}
 }
--- views/adminManagement.php
+++ views/adminManagement.php
@@ -29,5 +29,54 @@
 			</div>
 		</div>
 	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h3 class="panel-title text-center">Nettoyer les données annuelles</h3>
+				</div>
+				<div class="panel-body">
+					<form action="index.php?action=adminManagement" method="post">
+						<?php 
+						/*
+						<h4 class="text-center">
+							Etudiants
+							<input type="radio" name="wipeChoice" value="students" />
+						</h4>
+						<h4 class="text-center">
+							Présences
+							<input type="radio" name="wipeChoice" value="presences" />
+						</h4>
+						<h4 class="text-center">
+							Séries
+							<input type="radio" name="wipeChoice" value="serie" />
+						</h4>
+						<h4 class="text-center">
+							UE/AA
+							<input type="radio" name="wipeChoice" value="lessons" />
+						</h4>
+						<h4 class="text-center">
+							Professeurs
+							<input type="radio" name="wipeChoice" value="teachers" />
+						</h4>
+						<h4 class="text-center">
+							Agenda
+							<input type="radio" name="wipeChoice" value="agenda" />
+						</h4>
+						*/
+						?>
+						<h4 class="text-center">
+							Carwash
+							<input type="radio" name="wipeChoice" value="total" />
+						</h4>
+						<div class="text-center">
+							<input type="submit" value="Nettoyer la sélection" />
+						</div>
+					</form>
+		  		</div>
+			</div>
+		</div>
+	</div>
 
 </section>
