26b9aebe774fe66983fbea421c667772c8ce86c5
marcoamory <amorymarco@gmail.com>
2017-05-05 08:34:55 +0200

    Debut page de présence

---
+++ views/teacher.php
@@ -0,0 +1,38 @@
+<section id='teacher_view' class="container-fluid">
+<div class="row">
+	<div class="panel panel-default">
+	  <div class="panel-body">
+	    	<form method="post" action="index.php?action=teacher">
+	    		<div class="col-md-3 col-md-offset-1">
+	    			<select class="form-control">
+	    				<option value="bloc1">Bloc 1</option>
+	    				<option value="bloc2">Bloc 2</option>
+	    				<option value="bloc3">Bloc 3</option>
+	    			</select>
+	    		</div>
+	    		<div class="col-md-3">
+	    			<select class="form-control">
+	    				<option value="1">Serie 1</option>
+	    				<option value="2">Serie 2</option>
+	    				<option value="3">Serie 3</option>
+	    				<option value="4">Serie 4</option>
+	    				<option value="5">Serie 5</option>
+	    				<option value="6">Serie 6</option>
+	    			</select>
+	    		</div>
+	    		<div class="col-md-3">
+	    			<select class="form-control">
+	    				<option>COURS</option>
+	    			</select>
+	    		</div>
+	    		<div class="col-md-1">
+	    			<input type=submit class="btn btn-success" value="Rechercher"/>
+	    		</div>
+	    	</form>
+	  </div>
+	</div>
+</div>
+
+
+
+</section>
